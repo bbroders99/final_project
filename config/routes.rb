11b7d3046d1f9cb8b421e7038d56d7aeeb347648
Ben Morales <b-morales@kellogg.northwestern.edu>
2013-10-20 20:57:18 -0500

    Establish 'home' and 'post_idea' pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ FinalProject::Application.routes.draw do
 
   get("/home", { :controller => "pages", :action => "home_method" })
 
+  get("/post_idea", { :controller => "pages", :action => "post_idea_method" })
+
 end
