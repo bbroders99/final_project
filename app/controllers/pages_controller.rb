76f694dfda7dae6267ec260c875fe011692b1855
Ben Morales <b-morales@kellogg.northwestern.edu>
2013-10-30 23:00:48 -0500

    Resync

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,4 @@ class PagesController < ApplicationController
 	def home_method
 		render("home_method")
 	end
-
-	def post_idea_method
-		render("post_idea_method")
-	end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ FinalProject::Application.routes.draw do
 
   get("/home", { :controller => "pages", :action => "home_method" })
 
-  get("/post_idea", { :controller => "pages", :action => "post_idea_method" })
-
 end
